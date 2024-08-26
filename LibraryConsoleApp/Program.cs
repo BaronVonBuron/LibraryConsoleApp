c4f8eceb928da3c8946f153fa42a75f2cfc41b33
BaronVonBuron <andbur01@easv365.dk>
2024-08-26 09:35:42 +0200

    Refactored

--- LibraryConsoleApp/Program.cs
+++ LibraryConsoleApp/Program.cs
@@ -22,49 +22,16 @@ while (running)
     switch (input)
     {
         case "1":
-            Console.Write("Enter the title: ");
-            string title = Console.ReadLine();
-            Console.Write("Enter the author: ");
-            string author = Console.ReadLine();
-            Console.Write("Enter the year: ");
-            int year = int.Parse(Console.ReadLine());
-            Console.Write("Enter the ISBN: ");
-            string isbn = Console.ReadLine();
-            Book book = new(title, author, year, isbn, false);
-            library.AddBook(book);
-            Console.WriteLine("Book added");
-            UpdateLibrary();
+            CreateNewBook();
             break;
         case "2":
-            Console.WriteLine("Enter the ISBN of the book you want to delete");
-            string ISBN = Console.ReadLine();
-            library.DeleteBook(ISBN);
-            Console.WriteLine("Book deleted");
-            UpdateLibrary();
+            DeleteBook();
             break;
         case "3":
-            Console.WriteLine("Enter the ISBN of the book you want to update");
-            string isbnToUpdate = Console.ReadLine();
-            Console.Write("Enter the title: ");
-            string titleToUpdate = Console.ReadLine();
-            Console.Write("Enter the author: ");
-            string authorToUpdate = Console.ReadLine();
-            Console.Write("Enter the year: ");
-            int yearToUpdate = int.Parse(Console.ReadLine());
-            Console.Write("Enter the ISBN: ");
-            string isbnNew = Console.ReadLine();
-            Book bookToUpdate = new(titleToUpdate, authorToUpdate, yearToUpdate, isbnNew, false);
-            library.UpdateBook(isbnToUpdate, bookToUpdate);
-            Console.WriteLine("Book updated");
-            UpdateLibrary();
+            UpdateBook();
             break;
         case "4":
-            Console.WriteLine("Enter the ISBN of the book you want to borrow");
-            string isbnToBorrow = Console.ReadLine();
-            Console.WriteLine("Enter the name of the user");
-            string name = Console.ReadLine();
-            library.BorrowBook(isbnToBorrow, name);
-            UpdateLibrary();
+            BorrowABook();
             break;
         case "5":
             UpdateLibrary();
@@ -74,10 +41,7 @@ while (running)
             running = false;
             break;
         case "7":
-            foreach (var libraryBook in library.Books)
-            {
-                Console.WriteLine(libraryBook.ToString());
-            }    
+            ListAllBooks();
             break;
         case "8":
             UserMenu();
@@ -87,6 +51,69 @@ while (running)
             break;
     }
 }
+
+void ListAllBooks()
+{
+    foreach (var libraryBook in library.Books)
+    {
+        Console.WriteLine(libraryBook.ToString());
+    }
+}
+
+void BorrowABook()
+{
+    Console.WriteLine("Enter the ISBN of the book you want to borrow");
+    string isbnToBorrow = Console.ReadLine();
+    Console.WriteLine("Enter your name");
+    string userName = Console.ReadLine();
+    library.BorrowBook(isbnToBorrow, userName);
+    Console.WriteLine("Book borrowed");
+    UpdateLibrary();
+}
+
+void UpdateBook()
+{
+    Console.WriteLine("Enter the ISBN of the book you want to update");
+    string isbnToUpdate = Console.ReadLine();
+    Console.Write("Enter the title: ");
+    string titleToUpdate = Console.ReadLine();
+    Console.Write("Enter the author: ");
+    string authorToUpdate = Console.ReadLine();
+    Console.Write("Enter the year: ");
+    int yearToUpdate = int.Parse(Console.ReadLine());
+    Console.Write("Enter the ISBN: ");
+    string isbnNew = Console.ReadLine();
+    Book bookToUpdate = new(titleToUpdate, authorToUpdate, yearToUpdate, isbnNew, false);
+    library.UpdateBook(isbnToUpdate, bookToUpdate);
+    Console.WriteLine("Book updated");
+    UpdateLibrary();
+}
+
+void CreateNewBook()
+{
+    Console.Write("Enter the title: ");
+    string title = Console.ReadLine();
+    Console.Write("Enter the author: ");
+    string author = Console.ReadLine();
+    Console.Write("Enter the year: ");
+    int year = int.Parse(Console.ReadLine());
+    Console.Write("Enter the ISBN: ");
+    string isbn = Console.ReadLine();
+    Book book = new(title, author, year, isbn, false);
+    library.AddBook(book);
+    Console.WriteLine("Book added");
+    UpdateLibrary();
+}
+
+void DeleteBook()
+{
+    Console.WriteLine("Enter the ISBN of the book you want to delete");
+    string ISBN = Console.ReadLine();
+    library.DeleteBook(ISBN);
+    Console.WriteLine("Book deleted");
+    UpdateLibrary();
+}
+
 void UpdateLibrary(){
     library.Save(path);
     library.Load(path);
@@ -107,46 +134,16 @@ void UserMenu()
         switch (input)
         {
             case "1":
-                Console.Write("Enter the name: ");
-                string name = Console.ReadLine();
-                Console.Write("Enter the email: ");
-                string email = Console.ReadLine();
-                Console.Write("Enter the address: ");
-                string address = Console.ReadLine();
-                Console.Write("Enter the phone number: ");
-                string phoneNumber = Console.ReadLine();
-                LibraryUser user = new(name, email, address, phoneNumber);
-                library.Users.Add(user);
-                Console.WriteLine("User added");
+                AddAUser();
                 break;
             case "2":
-                Console.WriteLine("Enter the name of the user you want to delete");
-                string nameToDelete = Console.ReadLine();
-                library.Users.RemoveAll(user => user.Name == nameToDelete);
-                Console.WriteLine("User deleted");
+                DeleteAUser();
                 break;
             case "3":
-                Console.WriteLine("Enter the name of the user you want to update");
-                string nameToUpdate = Console.ReadLine();
-                Console.Write("Enter the name: ");
-                string nameNew = Console.ReadLine();
-                Console.Write("Enter the email: ");
-                string emailNew = Console.ReadLine();
-                Console.Write("Enter the address: ");
-                string addressNew = Console.ReadLine();
-                Console.Write("Enter the phone number: ");
-                string phoneNumberNew = Console.ReadLine();
-                LibraryUser userToUpdate = new(nameNew, emailNew, addressNew, phoneNumberNew);
-                library.Users.RemoveAll(user => user.Name == nameToUpdate);
-                library.Users.Add(userToUpdate);
-                Console.WriteLine("User updated");
+                UpdateAUser();
                 break;
             case "4":
-                foreach (var libraryUser in library.Users)
-                {
-                    Console.WriteLine(libraryUser.ToString());
-                }
-
+                ListAllUsers();
                 break;
             case "5":
                 usermenu = false;
@@ -156,4 +153,52 @@ void UserMenu()
                 break;
         }
     }
+}
+void ListAllUsers()
+{
+    foreach (var libraryUser in library.Users)
+    {
+        Console.WriteLine(libraryUser.ToString());
+    }
+}
+
+void UpdateAUser()
+{
+    Console.WriteLine("Enter the name of the user you want to update");
+    string nameToUpdate = Console.ReadLine();
+    Console.Write("Enter the name: ");
+    string nameNew = Console.ReadLine();
+    Console.Write("Enter the email: ");
+    string emailNew = Console.ReadLine();
+    Console.Write("Enter the address: ");
+    string addressNew = Console.ReadLine();
+    Console.Write("Enter the phone number: ");
+    string phoneNumberNew = Console.ReadLine();
+    LibraryUser userToUpdate = new(nameNew, emailNew, addressNew, phoneNumberNew);
+    library.Users.RemoveAll(user => user.Name == nameToUpdate);
+    library.Users.Add(userToUpdate);
+    Console.WriteLine("User updated");
+}
+
+void DeleteAUser()
+{
+    Console.WriteLine("Enter the name of the user you want to delete");
+    string nameToDelete = Console.ReadLine();
+    library.Users.RemoveAll(user => user.Name == nameToDelete);
+    Console.WriteLine("User deleted");
+}
+    
+void AddAUser()
+{
+    Console.Write("Enter the name: ");
+    string name = Console.ReadLine();
+    Console.Write("Enter the email: ");
+    string email = Console.ReadLine();
+    Console.Write("Enter the address: ");
+    string address = Console.ReadLine();
+    Console.Write("Enter the phone number: ");
+    string phoneNumber = Console.ReadLine();
+    LibraryUser user = new(name, email, address, phoneNumber);
+    library.Users.Add(user);
+    Console.WriteLine("User added");
 }
